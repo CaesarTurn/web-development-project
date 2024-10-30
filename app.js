3eebdb74b4cd7350b8dc3342035cc615174341d8
CaesarTurn <Cturn193@students.kennesaw.edu>
2024-10-30 14:54:06 -0400

    Update app.js

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.post('https://caesarturn.github.io/web-development-project/Subscribed.html', (req, res) => {
+app.post('Subscribed.html', (req, res) => {
     const username = req.body.username;
     res.send('Username is $cturn193');
 });
 
 app.listen(port, () => {
-    console.log('Server running on https://caesarturn.github.io/web-development-project/Subscribed.html');
+    console.log('Server running on https://caesarturn.github.io/web-development-project/');
 });
